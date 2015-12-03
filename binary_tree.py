b4d8400e84546096ce33994e8158496f1fb7482a
illusion <sdillusion@users.noreply.github.com>
2015-12-03 10:10:35 +0800

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -8,13 +8,15 @@ Output:
 '''
 
 def binary_search(lst, lo, hi, target):
-    if hi - lo <= 1:
-        return lst[lo] == target or lst[hi] == target
-    mid = (hi + lo)/2 
+    if hi - lo ==0:
+        return lo
+    if hi - lo < 0:
+        return flase
+    mid = (hi + lo)//2
     if target < lst[mid]:
-        return binary_search(lst, lo, mid, target)
+        return binary_search(lst, lo, mid-1, target)
     if target > lst[mid]:
-        return binary_search(lst, mid, hi, target)
+        return binary_search(lst, mid+1, hi, target)
     return mid
 
 print(binary_search([1, 4, 7, 8], 0, 3, 4))
